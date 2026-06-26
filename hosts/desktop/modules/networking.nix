32198b2c471c082a952c6c3d2a10a043ee9833be
emilysoft <aura.git@proton.me>
2026-06-26 19:40:24 -0400

    fix(network): fix network settings properly

--- hosts/desktop/modules/networking.nix
+++ hosts/desktop/modules/networking.nix
@@ -1,14 +1,7 @@
 {pkgs, ...}: {
   networking = {
     networkmanager.enable = true;
-
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    # interfaces.enp2s0.useDHCP = lib.mkDefault true;
-    useDHCP = false; # Importante para evitar conflictos con NetworkManager
-
+    nameservers = ["8.8.8.8" "1.1.1.1"];
     hostName = "nixos";
     firewall = {
       enable = true;
@@ -19,10 +12,8 @@
   };
 
   programs.nm-applet.enable = true;
-
   environment.systemPackages = with pkgs; [
     polkit_gnome
-    networkmanagerapplet
   ];
 
   security.polkit.enable = true;
