7e38a70450b087cee4916e4aaf4f46a57e49eb1c
emilysoft <aura.git@proton.me>
2026-08-27 18:02:07 -0400

    refactor(pawncc): remove pawncc

--- home/nit/programs/overlays.nix
+++ home/nit/programs/overlays.nix
@@ -1,7 +1,6 @@
 {inputs}: final: prev: {
   sorter = final.callPackage ./utilities/sorter.nix {};
   recorder = final.callPackage ./media/recorder.nix {};
-  pawncc = final.callPackage ./developing/pawncc.nix {};
   assets = final.callPackage ./assets.nix {};
   manhattan-cafe = final.callPackage ./desktop-environment/manhattan-cafe.nix {};
   lutris =
