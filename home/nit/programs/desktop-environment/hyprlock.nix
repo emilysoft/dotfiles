67f0f48fc9e48a3e7020cc757127ee501d3cb872
emilysoft <aura.git@proton.me>
2026-09-10 14:57:57 -0400

    fix(hyprlock): fix date path and change time format

--- home/nit/programs/desktop-environment/hyprlock.nix
+++ home/nit/programs/desktop-environment/hyprlock.nix
@@ -23,7 +23,7 @@
         # RELOJ (Gigante y grueso)
         {
           monitor = "";
-          text = "$TIME";
+          text = ''cmd[update:10000] echo "$(/run/current-system/sw/bin/date +"%I:%M %p")"'';
           color = "rgba(255, 255, 255, 1.0)";
           font_size = 100;
           font_family = "Inter ExtraBold";
@@ -38,7 +38,7 @@
         # FECHA (Arriba del reloj)
         {
           monitor = "";
-          text = ''cmd[update:10000] echo "$(d/run/current-system/sw/bin/date +"%a %d %b.")"'';
+          text = ''cmd[update:10000] echo "$(/run/current-system/sw/bin/date +"%a %d %b.")"'';
           color = "rgba(255, 255, 255, 1.0)";
           font_size = 18;
           font_family = "Inter Bold";
