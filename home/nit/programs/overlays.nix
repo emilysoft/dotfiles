2a03ac74e63e215d64b0d4c3b22a31a88e9953d1
emilysoft <aura.git@proton.me>
2026-09-18 16:52:10 -0400

    chore9(overlays): improve implementation

--- home/nit/programs/overlays.nix
+++ home/nit/programs/overlays.nix
@@ -1,11 +1,13 @@
-{inputs}: final: prev: {
+{inputs, ...}: final: prev: let
+  pkgs-stable = import inputs.nixpkgs-stable {
+    inherit (prev) system;
+    config = builtins.removeAttrs prev.config ["rewriteURL"];
+  };
+in {
   sorter = final.callPackage ./utilities/sorter.nix {};
   recorder = final.callPackage ./media/recorder.nix {};
   assets = final.callPackage ./assets.nix {};
   manhattan-cafe = final.callPackage ./desktop-environment/manhattan-cafe.nix {};
-  lutris =
-    (import inputs.nixpkgs-stable {
-      inherit (prev) system;
-      config.allowUnfree = true;
-    }).lutris;
+
+  lutris = pkgs-stable.lutris;
 }
